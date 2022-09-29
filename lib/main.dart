fe45d883627e8237e05a196b1961f08abcfcdc78
FlutLab.io <>
2022-09-29 19:31:31 +0000

    finalizacao_29-09

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,121 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Stateful Clicker Counter',
-      theme: ThemeData(
-        // Application theme data, you can set the colors for the application as
-        // you want
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Clicker Counter Home Page'),
-    );
-  }
+void main() {
+  runApp(
+    const MaterialApp(
+      title: "App",
+      home: PaginaVendas(),
+    ),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class PrimeiroApp extends StatefulWidget {
+  const PrimeiroApp({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<PrimeiroApp> createState() => _PrimeiroAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _PrimeiroAppState extends State<PrimeiroApp> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Meu App Novo"),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 25),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.account_circle),
+                      hintText: "Digite o seu login",
+                      labelText: "Seu Login")),
+              TextFormField(
+                decoration: InputDecoration(
+                    icon: Icon(Icons.password),
+                    hintText: "Digite a sua senha",
+                    labelText: "Senha"),
+                obscureText: true,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(15.0),
+                  width: 110,
+                  height: 60,
+                  child: ElevatedButton(onPressed: () {}, child: Text("Logar")))
+            ],
+          )),
     );
+  }
+}
+
+class PaginaVendas extends StatefulWidget {
+  const PaginaVendas({Key? key}) : super(key: key);
+
+  @override
+  State<PaginaVendas> createState() => _PaginaVendasState();
+}
+
+class _PaginaVendasState extends State<PaginaVendas> {
+  List<Jogos> jogos = [
+    Jogos("Gta", 150.0, "lib/imagens/Gta5-Capa.png"),
+    Jogos("PES", 250.0, "lib/imagens/Efootbol_22-Capa.png"),
+    Jogos("God Of War", 250.0, "lib/imagens/God-of-war-Capa.jpg"),
+    Jogos("Fifa", 250.0, "lib/imagens/Fifa22-Capa.png"),
+    Jogos("Fortnite", 0.0, "lib/imagens/Fortnite-Capa.jpg"),
+    Jogos("Call Of Dute", 170.0, "lib/imagens/Call-of-Duty-Capa.jpg"),
+    Jogos("Red Dead Redemption", 300.0, "lib/imagens/Gta5-Capa.png"),
+    Jogos("Spider-Man", 300.0, "lib/imagens/Spider-Man_capa.png")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          title: Text("Melhores Jogos"),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: jogos.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: Image.asset(jogos[index].imagem),
+                title: Text(jogos[index].nome),
+                subtitle: Text("R\$" + jogos[index].preco.toStringAsFixed(2)),
+                trailing:
+                    ElevatedButton(onPressed: () {}, child: Text("Comprar")),
+              ),
+            );
+          },
+        ));
+  }
+}
+
+class Jogos {
+  String nome = "";
+  double preco = 0.0;
+  String imagem = "";
+
+  Jogos(String _jogo, double _preco, String _imagem) {
+    nome = _jogo;
+    preco = _preco;
+    imagem = _imagem;
   }
 }
